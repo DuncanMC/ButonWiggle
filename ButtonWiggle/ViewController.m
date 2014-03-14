b55e90fbac49bdc081f50f09303576939d0bb4ce
Duncan Champney <dchampney@wareto.com>
2014-03-14 14:50:29 -0400

    More tweaks, plus added a .gitignore file.

--- ButtonWiggle/ViewController.m
+++ ButtonWiggle/ViewController.m
@@ -14,6 +14,7 @@
 
 @implementation ViewController
 
+//-----------------------------------------------------------------------------------------------------------
 - (CGFloat) randomFloat: (CGFloat) max_value;
 {
 	CGFloat aRandom = (arc4random() % 1000000001);
@@ -29,11 +30,15 @@
 	return aRandom;
 }
 
+//-----------------------------------------------------------------------------------------------------------
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+//-----------------------------------------------------------------------------------------------------------
 
 - (void)didReceiveMemoryWarning
 {
@@ -41,27 +46,33 @@
     // Dispose of any resources that can be recreated.
 }
 
+//-----------------------------------------------------------------------------------------------------------
+
 //UIViewAnimationOptionAutoreverse | UIViewAnimationOptionRepeat
 - (void) handleWiggle;
 {
   //Randomize the animation speed for each repeat
-  CGFloat random = [self randomFloat: 1.5];
+  CGFloat random = [self randomFloat: .5];
   _jellybeanButton.layer.speed = 1 + random;
- static CGFloat duration = .39;
+  
+  
+  //randomize the duration of both steps slightly
+  CGFloat duration = .18 + [self randomFloatPlusOrMinus: .02];
   
   [UIView animateWithDuration: duration
                         delay: 0
-       usingSpringWithDamping: .08
-        initialSpringVelocity: .667
-                      options: UIViewAnimationOptionCurveLinear| UIViewAnimationOptionAllowUserInteraction
+   options:  UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionAllowUserInteraction
                    animations: ^
    {
+     //Pick an amount to change the height & width of the iamge
      CGFloat delta= .05 + [self randomFloat: .05];
      if (arc4random_uniform(2) == 0)
        delta *= -1;
      
-     CGAffineTransform transform = CGAffineTransformMakeScale(1 + delta, 1 -delta); //Make the button image wider and shorter or narrower and taller.
+     //Create a transform that scales the image
+     CGAffineTransform transform = CGAffineTransformMakeScale(1 + delta, 1 -delta); //Make the image wider and shorter or narrower and taller.
      
+     //-----
      //Rotate it by a random amount around a slightly randomized center-point
      CGFloat x,y;
      x = .5 + [self randomFloatPlusOrMinus: 20];
@@ -70,38 +81,42 @@
      CGFloat rotation;
      rotation = [self randomFloatPlusOrMinus: .2];
      
+     //To rotate around a random point,
+     //Shift the enter point, rotate, and shift the center point back
      transform = CGAffineTransformTranslate(transform, x, y);
      transform = CGAffineTransformRotate(transform, rotation * M_2_PI);
      transform = CGAffineTransformTranslate(transform, -x, -y);
+     //-----
      _jellybeanButton.transform = transform;
    }
                    completion: ^(BOOL finished)
    //When the animation is done, animate the view back to normal
    {
-     CGFloat random = [self randomFloat: 1.2];
+     CGFloat random = [self randomFloat: .5];
      _jellybeanButton.layer.speed = 1 + random;
      
      [UIView animateWithDuration: duration
                            delay: 0
-          usingSpringWithDamping: .1
-           initialSpringVelocity: .667
-                         options: UIViewAnimationOptionCurveLinear | UIViewAnimationOptionAllowUserInteraction
+                         options:  UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionAllowUserInteraction
                       animations: ^
       {
+        //Set the view's trnaform back to the identity transform.
         CGAffineTransform transform = CGAffineTransformIdentity;
         _jellybeanButton.transform = transform;
       }
                       completion: ^(BOOL finished)
       {
         //Now start the process over agaon.
+        
         if (_wiggleSwitch.isOn)
-        [self performSelector: @selector(handleWiggle)  withObject: nil afterDelay: .02];
+          [self handleWiggle];
       }
       ];
    }
    ];
 }
 
+//-----------------------------------------------------------------------------------------------------------
 
 - (IBAction)handleWiggleSwitch:(UISwitch *)sender
 {
