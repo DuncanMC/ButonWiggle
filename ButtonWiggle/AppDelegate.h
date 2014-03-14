e92aa5a0b969ec9e68d0dbe72d8eae07de06ac5b
Duncan Champney <dchampney@wareto.com>
2014-03-14 12:09:49 -0400

    Initial Commit

---
+++ ButtonWiggle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ButtonWiggle
+//
+//  Created by Duncan Champney on 3/14/14.
+//  Copyright (c) 2014 WareTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ButtonWiggleTests/ButtonWiggleTests.m
@@ -0,0 +1,34 @@
+//
+//  ButtonWiggleTests.m
+//  ButtonWiggleTests
+//
+//  Created by Duncan Champney on 3/14/14.
+//  Copyright (c) 2014 WareTo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ButtonWiggleTests : XCTestCase
+
+@end
+
+@implementation ButtonWiggleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
